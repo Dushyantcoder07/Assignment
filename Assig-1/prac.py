3c9b723b1afa1e2f2428b617545dba02013cf8bd
Dushyantcoder07 <dushyantsisodiya18@gmail.com>
2026-06-01 09:13:36 +0530

    ass-1

---
+++ Assig-1/prac.py
@@ -0,0 +1,11 @@
+# 1) Practice print statement: - with multiple object - with seperator - with "ends" 
+# 2) Using input ask user to enter his/her name and output user name with a greeting message
+
+# Practice print statement
+print("Hello", "World", sep=" - ", end="!\n")
+print("Welcome to Python programming.", end=" ")
+print("Let's learn together.")
+
+# Using input to ask user for their name and output a greeting message
+name = input("Please enter your name: ")
+print(f"Hello, {name}! Welcome to the world of Python programming!")
